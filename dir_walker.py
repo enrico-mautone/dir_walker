dea28cf09349744097c56122f8655c07f0b4ac34
Enrico <enrico.mautone@remira.com>
2024-07-05 14:12:19 +0200

    added -d param to go command

--- dir_walker.py
+++ dir_walker.py
@@ -27,7 +27,7 @@ def get_directory_size(path):
                 total_size += os.path.getsize(fp)
     return total_size
 
-def display_directory_tree(path):
+def display_directory_tree(path, only_dirs=False):
     dir_size = get_directory_size(path)
     size_str = format_size(dir_size)
     tree = Tree(f"{path} {f'({size_str})' if size_str else ''}", guide_style="bold bright_blue")
@@ -40,7 +40,7 @@ def display_directory_tree(path):
                 size_str = format_size(dir_size)
                 branch = tree.add(f"[bold magenta]{item}/[/bold magenta] {f'({size_str})' if size_str else ''}")
                 add_directory(branch, full_path)
-            else:
+            elif not only_dirs:
                 size = os.path.getsize(full_path)
                 size_str = format_size(size)
                 tree.add(f"[bold green]{item}[/bold green] {f'({size_str})' if size_str else ''}")
@@ -54,7 +54,7 @@ def cli():
 
     while True:
         command = console.input(f"[bold cyan]Current path: {current_path}[/bold cyan]\n"
-                                "[bold cyan]Command ([/bold cyan][yellow]cd[/yellow] [bold cyan]path, [bold cyan][yellow]..[/yellow][bold cyan], [bold cyan][yellow]go[/yellow][bold cyan], [bold cyan][yellow]cls[/yellow][bold cyan], [bold cyan][yellow]exit[/yellow][bold cyan]): [/bold cyan]")
+                                "[bold cyan]Command ([/bold cyan][yellow]cd[/yellow] [bold cyan]path, [bold cyan][yellow]..[/yellow][bold cyan], [bold cyan][yellow]go[/yellow][bold cyan] [dim yellow]-d[/dim yellow], [bold cyan][yellow]cls[/yellow][bold cyan], [bold cyan][yellow]exit[/yellow][bold cyan]): [/bold cyan]")
 
         if command.startswith("cd "):
             new_path = command[3:].strip()
@@ -70,15 +70,16 @@ def cli():
                 console.print(f"[bold green]Changed directory to {current_path}[/bold green]")
             else:
                 console.print(f"[bold red]Error:[/bold red] '{parent_path}' is not a valid directory")
-        elif command == "go":
-            display_directory_tree(current_path)
+        elif command.startswith("go"):
+            only_dirs = "-d" in command
+            display_directory_tree(current_path, only_dirs)
         elif command == "cls":
             console.clear()
         elif command == "exit":
             console.print("[bold cyan]Exiting...[/bold cyan]")
             break
         else:
-            console.print("[bold red]Invalid command.[/bold red] Please use 'cd <path>', '..', 'go', 'cls', or 'exit' to exit.")
+            console.print("[bold red]Invalid command.[/bold red] Please use 'cd <path>', '..', 'go [-d]', 'cls', or 'exit' to exit.")
 
 if __name__ == '__main__':
     cli()
